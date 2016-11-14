eaf2bb24635a15b5172ecc4dac79dd8b97df5e00
Renno <rennosmx@hotmail.com>
2016-11-14 14:59:06 -0300

    Criação Tela de Login

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,24 +1,53 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import view.LoginController;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	
+	private Stage primaryStage;
+	private AnchorPane telaLogin;
+	
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) {		
+		
+		this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("SGP - Saúde");
+        
+        mostraTelaLogin();			
+	}
+	
+	
+public void mostraTelaLogin(){
+		
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
+			 // Carrega a tela de login
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("/view/Login.fxml"));					
+			telaLogin = (AnchorPane) loader.load();
+			
+			Scene scene = new Scene(telaLogin);
+            primaryStage.setScene(scene);
+            primaryStage.setResizable(false);            
+            primaryStage.show();
+			                      
+            LoginController controller = loader.getController();
+            controller.setMain(this);
+            
+            
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+	
 	
 	public static void main(String[] args) {
 		launch(args);
---
+++ src/view/LoginController.java
@@ -0,0 +1,31 @@
+package view;
+
+import application.Main;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+	private Main main;
+	
+	@FXML
+	TextField txUsuario;
+	
+	@FXML
+	TextField txSenha;
+	
+	@FXML
+	public void btnEntrar(){
+		
+	}
+		
+	@FXML
+	public void linkCadastrar(){
+		
+	}
+
+	public void setMain(Main main) {
+		
+	}
+			
+}
