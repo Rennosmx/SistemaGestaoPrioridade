ecaeba878078273e9997c274055cdc04869f7b22
Renno <rennosmx@hotmail.com>
2016-11-16 16:04:34 -0300

    Iniciando criação de Tela de Cadastros

---
+++ src/model/Usuario.java
@@ -0,0 +1,44 @@
+package model;
+
+import java.util.Date;
+
+public class Usuario {
+
+	private int id;	
+	private String login;
+	private String senha;
+	private Date dataNascimento;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getLogin() {
+		return login;
+	}
+	
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	
+	public String getSenha() {
+		return senha;
+	}
+	
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+}
