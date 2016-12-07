cea0f98cdba24d93935cd5a79947be30c69320a5
johnmelo <jonathas_ruam@hotmail.com>
2016-12-07 03:23:34 -0300

    code DAOMedico

--- src/DAO/DAOMedico.java
+++ src/DAO/DAOMedico.java
@@ -6,7 +6,9 @@ public class DAOMedico extends DBConect {
 	public static final String COLUMN_USER_ID = "usuario_id";
 	public static final String COLUMN_CRM = "crm";
 	public static final String COLUMN_ESPECIALIDADE = "especialidade";
-	private static final String CREATE_TABLE = "";
+	private static final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS "+TABLE_NAME
+			+ "("+COLUMN_ID+" serial, "+COLUMN_USER_ID+" integer NOT NULL, "+COLUMN_CRM+" varchar(20) NOT NULL, "
+			+COLUMN_ESPECIALIDADE+" varchar(50) NOT NULL, enabled integer NOT NULL DEFAULT = 1, PRIMARY KEY(id)";
 	
 	public DAOMedico(){
 		super();
